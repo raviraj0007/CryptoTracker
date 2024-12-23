c9928917d90ddb63740d87af2cfc6ea135ba1f75
raviraj0007 <raviraj04066@gmail.com>
2024-12-23 20:30:38 +0530

    Coin List Screen - 2 - Network Layer Utility

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUI.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/models/CoinUI.kt
@@ -1,10 +1,8 @@
 package com.plcoding.cryptotracker.crypto.presentation.models
 
 import androidx.annotation.DrawableRes
-import androidx.compose.ui.text.intl.Locale
 import com.plcoding.cryptotracker.crypto.domain.Coin
-import com.plcoding.cryptotracker.util.getDrawableIdForCoin
-import java.text.NumberFormat
+import com.plcoding.cryptotracker.core.presentation.util.getDrawableIdForCoin
 import java.util.Locale.getDefault
 
 data class CoinUI(
